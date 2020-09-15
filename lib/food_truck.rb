4edb3a96a7a13456f6265efa30037d1d2c70572b
Cydnee Owens <cks410@yahoo.com>
2020-09-15 14:20:12 -0600

    'Add attributes item and food truck stock and inventory

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -1,7 +1,32 @@
 class FoodTruck
-  attr_reader :name
+  attr_reader :name,
+              :inventory,
+              :stock_qty
 
   def initialize(name)
     @name = name
+    @inventory = {}
+    @stock_qty = {}
+  end
+
+  def check_stock(item)
+    stock = 0
+    if @stock_qty[item].nil?
+      count = 0
+    else
+      count += item
+    end
+  end
+
+  def stock(item, quantity)
+    @stock_qty.each do |item, quantity|
+      if @inventory[item].nil?
+        @inventory[item] = quantity
+      else
+        @inventory[item] += quantity
+      end
+    end
+binding.pry
+    @stock_qty
   end
 end
--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -15,4 +15,32 @@ class FoodTruckTest < Minitest::Test
 
     assert_equal "Rocky Mountain Pies", food_truck.name
   end
+
+  def test_it_can_add_inventory
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+
+    expected = {}
+    expected_1 = {item1 => 30}
+    expected_2 = {item1 => 55, item2 => 12}
+
+    assert_equal expected, food_truck.inventory
+
+    assert_equal 0, food_truck.check_stock(item1)
+
+    food_truck.stock(item1, 30)
+
+    assert_equal expected_1, food_truck.inventory
+
+    assert_equal 30, food_truck.check_stock(item1)
+
+    food_truck.stock(item1, 25)
+
+    assert_equal 55, food_truck.check_stock(item1)
+
+    food_truck.stock(item2, 12)
+
+    assert_equal expected_1, food_truck.inventory
+  end
 end
